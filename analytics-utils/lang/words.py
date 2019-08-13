7d143c924875d7263cbebaee90b44c98a37dfbd5
patricksferraz <patrick.ferraz@outlook.com>
2019-08-13 12:48:41 -0300

    feat(lang/words): adds language support to portuguese words

---
+++ analytics-utils/lang/words.py
@@ -0,0 +1,19 @@
+words = {
+    "header": {"en": "header", "pt": "cabeçalho"},
+    "max": {"en": "max", "pt": "max"},
+    "min": {"en": "min", "pt": "min"},
+    "mean": {"en": "mean", "pt": "média"},
+    "median": {"en": "median", "pt": "mediana"},
+    "quantile": {
+        "en": lambda x: f"{x}-quantile",
+        "pt": lambda x: f"{x}-quartil",
+    },
+    "var": {"en": "variance", "pt": "variância"},
+    "std": {"en": "standard deviation", "pt": "desvio padrão"},
+    "mad": {"en": "absolute deviation", "pt": "desvio absoluto"},
+    "amp": {"en": "amplitude", "pt": "amplitude"},
+    "rms": {"en": "rms", "pt": "rms"},
+    "kurtosis": {"en": "kurtosis", "pt": "curtose"},
+    "skew": {"en": "skewness", "pt": "assimetria"},
+    "count": {"en": "count", "pt": "contagem"},
+}
