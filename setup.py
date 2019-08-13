e1a1be955db328d33318a21908d3ebb39f25a2d3
patricksferraz <patrick.ferraz@outlook.com>
2019-08-13 12:49:37 -0300

    chore(setup): adds package setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="analytics-utils",
+    version="0.1.dev",
+    author="Patrick Silva Ferraz, Leandro Souza de Oliveira",
+    author_email="patrick.ferraz@outlook.com",
+    description="Package with functions for data analytics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/patricksferraz/analytics-utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+)
