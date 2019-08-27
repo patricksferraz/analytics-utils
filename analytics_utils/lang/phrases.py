0f359dd7dd01b092e9bdb539997093cd0b888ae0
patricksferraz <patrick.ferraz@outlook.com>
2019-08-27 15:20:18 -0300

    feat(phrases): changes quantile from words to phrases

--- analytics_utils/lang/phrases.py
+++ analytics_utils/lang/phrases.py
@@ -2,5 +2,9 @@ phrases = {
     "unsupported": {
         "en": lambda x: f"unsupported {x}",
         "pt": lambda x: f"{x} não suportado(a)",
-    }
+    },
+    "quartile": {
+        "en": lambda x: f"{x}-quartile",
+        "pt": lambda x: f"{x}-quartil",
+    },
 }
