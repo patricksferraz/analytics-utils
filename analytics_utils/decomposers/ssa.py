87356489ea308d75e23c62af0f88d3deab4baebf
patricksferraz <patrick.ferraz@outlook.com>
2019-09-13 13:19:38 -0300

    fix(ssa): fix multivariate dataframe

--- analytics_utils/decomposers/ssa.py
+++ analytics_utils/decomposers/ssa.py
@@ -2,12 +2,7 @@
 """
 This is the find module.
 The find module supplies one function,
-    def ssa(
-        data_frame: pd.DataFrame,
-        window_size: int or float = 4,
-        groups: int or [int] = None,
-        headers: [str] = None,
-    ) -> pd.DataFrame
+    ssa()
 """
 
 from pyts.decomposition import SingularSpectrumAnalysis
@@ -27,42 +22,29 @@ def ssa(
         data_frame {pd.DataFrame} -- input dataframe
 
     Keyword Arguments:
-        window_size {int or float} -- Size of the sliding window (i.e. the size
-            of each word). If float, it represents the percentage of the size
-            of each time series and must be between 0 and 1. The window size
-            will be computed as
-            ``max(2, ceil(self.window_size * n_timestamps))`` (default: {4}).
-        groups {int or [int]} -- The way the elementary matrices are grouped.
-            If None, no grouping is performed. If an integer, it represents the
-            number of groups and the bounds of the groups are computed as
-            ``np.linspace(0, window_size, groups + 1).astype('int64')``.
-            If array-like, each element must be array-like and contain the
-            indices for each group. (default: {None}).
         headers {[str]} -- chosen dataframe headers (default: {None}).
+
+        {others params} -- See pyts.decomposition.SingularSpectrumAnalysis
 
     Returns:
         pd.DataFrame -- A object decomposed of length window_size for each
         feature (header).
     """
 
-    # changes columns to index
-    data_frame = data_frame.T
-    if headers:
-        data_frame = data_frame.loc[headers]
+    # Select and changes columns to index
+    if headers is None:
+        headers = data_frame.columns
+    else:
+        data_frame = data_frame[headers]
 
     # Generates window_size (default:4) lists for each feature
     ssa = SingularSpectrumAnalysis(window_size, groups)
-    x_ssa = ssa.fit_transform(data_frame)
+    x_ssa = ssa.fit_transform(data_frame.T)
 
     # Decompose each feature adding each window_size in line
-    decompose = {}
-    if len(headers) > 1:
-        for idx, h in enumerate(headers):
-            decompose[h] = [x for x in x_ssa[idx]]
-    else:
-        decompose[headers[0]] = [x for x in x_ssa]
+    decompose = [[x for x in x_ssa[i]] for i, _ in enumerate(headers)]
 
-    return pd.DataFrame(decompose)
+    return pd.DataFrame(decompose, index=headers).T
 
 
 if __name__ == "__main__":
@@ -86,28 +68,6 @@ if __name__ == "__main__":
         (default: 'columns')""",
     )
     ap.add_argument(
-        "-w",
-        "--window-size",
-        type=int or float,
-        default=2,
-        help="""Size of the sliding window (i.e. the size of each
-        word). If float, it represents the percentage of the size of each time
-        series and must be between 0 and 1. The window size will be computed as
-        ``max(2, ceil(self.window_size * n_timestamps))`` (default: 4}).""",
-    )
-    ap.add_argument(
-        "-g",
-        "--groups",
-        type=int,
-        nargs="*",
-        help="""The way the elementary matrices are grouped. If
-        None, no grouping is performed. If an integer, it represents the number
-        of groups and the bounds of the groups are computed as
-        ``np.linspace(0, window_size, groups + 1).astype('int64')``.
-        If array-like, each element must be array-like and contain the indices
-        for each group. (default: None).""",
-    )
-    ap.add_argument(
         "-pd",
         "--parse-dates",
         type=str,
@@ -129,13 +89,15 @@ if __name__ == "__main__":
         nargs="*",
         help="an string for the header in the dataset",
     )
+    ap.add_argument("--window-size", type=int or float, default=2)
+    ap.add_argument("--groups", type=int, nargs="*")
     args = vars(ap.parse_args())
 
     # If exist parse_dates, creates a structure with column name datetime
     if args["parse_dates"]:
         args["parse_dates"] = {"datetime": args["parse_dates"]}
 
-    # Apply ewm
+    # Apply ssa
     result = ssa(
         pd.read_csv(
             args["dataset"],
